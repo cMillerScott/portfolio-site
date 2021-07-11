28fbcef6534323ee719cc722d6ae20205defad67
Christopher <c.millerscott@gmail.com>
2021-07-10 22:41:36 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+function showLabel(labelId) {
+  const label = document.getElementById(labelId);
+  
+  label.style.visibility = "visible";
+  label.style.opacity = "1";
+  label.style.transform = "translateX(0rem) translateY(0rem)";
+}
+
+function hideLabel(labelId) {
+  const label = document.getElementById(labelId);
+  
+  label.style.visibility = "hidden";
+  label.style.opacity = "0";
+  label.style.transform = "translateX(1rem) translateY(2rem)";
+}
+
+// cycle through panels and set links
+function openPanel(evt, panelName, panelContent) {
+  const currentPanel = document.getElementById(panelName);
+  const currentPanelContent = document.getElementById(panelContent);
+
+  // sets panels to hidden
+  const panels = document.getElementsByClassName("panel");
+  for (let panel of panels) {
+    panel.style.display = "contents";
+    panel.className = panel.className.replace(" current", "");
+  }
+  // hides panel contents
+  const panelContents = document.getElementsByClassName("panel-content");
+  for (let panelCont of panelContents) {
+    panelCont.style.display = "none";
+    panelCont.className = panelCont.className.replace(" current", "");
+  }
+  // sets non-active links
+  const links = document.getElementsByClassName("link");
+  for (let link of links) {
+    link.className = link.className.replace(" active", "");
+  }
+
+  currentPanel.style.display = "flex";
+  currentPanel.className += " current";
+  currentPanelContent.style.display = "flex";
+  currentPanelContent.className += " current";
+  evt.currentTarget.className += " active";
+}
